fa210e6c5935d21d44d4ad6c92d3fcc0836e82ce
sergio <sebermudezg@unal.edu.co>
2024-04-06 16:46:07 -0500

    first commit

---
+++ src/main/java/com/sbz/springsecoauth2github/SpringSecOauth2GitHubApplication.java
@@ -0,0 +1,13 @@
+package com.sbz.springsecoauth2github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecOauth2GitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecOauth2GitHubApplication.class, args);
+    }
+
+}
